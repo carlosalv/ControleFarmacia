09c03f239beb59c59cf3562911120c414a411790
Carlos Alberto <allbertcarlo@gmail.com>
2017-08-11 21:05:00 -0300

    Projeto alterado

---
+++ src/br/com/farmacia/Usuario.java
@@ -0,0 +1,28 @@
+package br.com.farmacia;
+
+import java.util.ArrayList;
+
+public class Usuario {
+	int Id;
+	String nome;
+	String endereco;
+	String email;
+	byte receita;
+	
+	ArrayList<Medicamento> medicamento;
+	
+	public void consultarMedicamento(){
+			
+	
+		
+	}
+	
+	public void comprarMedicamento(){
+		
+	}
+	
+	public void efetuarPagamento(){
+		
+	}
+
+}
